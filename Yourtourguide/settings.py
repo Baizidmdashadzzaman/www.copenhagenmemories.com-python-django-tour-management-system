b557cd2a91ef01f52728c415d47b27ecab753900
Baizid MD Ashadzzaman <baizid.md.ashadzzaman@gmail.com>
2026-04-15 14:02:06 +0600

    04-14-2026

--- Yourtourguide/settings.py
+++ Yourtourguide/settings.py
@@ -172,8 +172,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.hostinger.com'
 #EMAIL_HOST = 'smtp.titan.email'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+# EMAIL_PORT = 465
+# EMAIL_USE_SSL = True
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
